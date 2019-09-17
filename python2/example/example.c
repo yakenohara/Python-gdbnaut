a8d4c71259734076dcf0d592f941b9e2eb6c4df8
yakenohara <38835246+yakenohara@users.noreply.github.com>
2019-09-17 21:19:19 +0900

    Pyhon2

--- python2/example/example.c
+++ python2/example/example.c
@@ -1,3 +1,12 @@
+/*
+gcc example.c -o example.exe -g -O0
+gdb example.exe -x example.py
+
+sys.path.append('.')
+import gdbnaut
+
+*/
+
 typedef unsigned int   t_uint32;
 typedef unsigned short t_uint16;
 typedef unsigned char  t_uint08;
@@ -89,6 +98,58 @@ typedef enum week {
 
 volatile e_week gsym2_e_week = Tue;
 
+//<test>
+
+volatile int gsym4 = (-1);
+volatile int *gsym4_p = &gsym4;
+volatile int **gsym4_pp = &gsym4_p;
+volatile void *gsym4_vp = &gsym4_p;
+
+typedef struct{
+    char   sym0_ch;
+    short  sym1_sh;
+    int    sym2_it;
+    long   sym3_lg;
+    float  sym4_fl;
+    double sym5_dl;
+}t_st_b;
+
+volatile t_st_b gsym1_t_st_b = {
+    'a',   //sym0_ch
+    1,     //sym1_sh
+    2,     //sym2_it
+    3,     //sym3_lg
+    11.2F, //sym4_fl
+    22.4F  //sym5_dl
+};
+
+int addition(int x, int y);       // 足し算
+int subtraction(int x, int y);    // 引き算
+
+int addition(int x, int y) {
+    return (x + y);
+}
+
+int subtraction(int x, int y) {
+    return (x - y);
+}
+
+void func_void(void){
+    addition(1,2);
+}
+
+int (*funcp)(int, int) = addition;
+void (*funcpv)(void) = func_void;
+void (*funcpvs)() = func_void;
+void (*funcpvs2) = func_void;
+void *funcpvs3 = func_void;
+
+volatile int (*funcpvol)(int, int) = addition;
+
+//</test>
+
 void main(void){
     return;
 }
+
+volatile void *main_p = main;
